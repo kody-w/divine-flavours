fa259b00c4290603b0ad7a52cae15b273e85fee3
Ana Demeny <ana.demeny@microsoft.com>
2020-07-03 13:24:21 +0100

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <div className="container mt-5">
             <div className="row">
-                Something else about this site...
+                Something else about this site... To be merged
             </div>
         </div>
     );
